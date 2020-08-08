eeb12df99a75e294ad3cb54780a2367f006cb409
tknemuru <kojikoji.max.5252@gmail.com>
2020-08-08 17:33:43 +0900

    add init.

---
+++ src/components/templates/MahjongTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    ほげ
+  </div>
+</template>
+
+<script>
+/**
+ * 麻雀卓
+ */
+export default {
+  name: 'MahjongTable',
+  components: {
+
+  },
+  props: {
+    sample: {
+      type: String,
+      default: null
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/store/modules/context.js
+++ src/store/modules/context.js
@@ -4,9 +4,65 @@ export default {
   namespaced: true,
   state () {
     return {
+      /**
+       * @description ルームキー
+       */
+      roomKey: null,
+      /**
+       * @description 自分の風を示すインデックス
+       */
+      windIndex: null,
+      /**
+       * @description 自分の風名称
+       */
+      wind: null,
+      /**
+       * @description 風とフィールド位置の組み合わせ情報
+       */
+      windPositions: {},
+      /**
+       * @description プレイヤキー
+       */
+      playerKey: null,
+      /**
+       * @description プレイヤ名
+       */
+      playerName: null,
+      /**
+       * @description 自分が初めて入室したプレイヤかどうか
+       */
+      firstPlayer: false,
+      /**
+       * @description ルーム名
+       */
+      roomName: null,
+      /**
+       * @description ルーム状態
+       */
+      roomState: null,
+      /**
+       * @description ターン
+       */
+      turn: null,
+      /**
+       * @description ルーム内のプレイヤ
+       */
+      players: {},
+      /**
+       * @description 手牌
+       */
+      hand: [],
+      /**
+       * @description 河牌
+       */
+      rivers: []
     }
   },
   actions: {
+    /**
+     * @description 即時開始処理を行います。
+     * @param {Object} param0 state
+     */
     async quickStart ({ state, commit }) {
       const body = {
         createRoomRequest: {
@@ -31,14 +87,45 @@ export default {
         selectRoomRequest: {
         }
       }
-      const result = await axios.post('http://localhost:25486/api/quickstart/', body)
-      return result
+      const response = await axios.post('http://localhost:25486/api/quickstart/', body)
+      commit('init', state, response.data)
+      return response
     }
   },
   mutations: {
-
+    /**
+     * @description 初期化処理を行います。
+     */
+    init (state, context) {
+      state.roomKey = context.roomKey
+      state.windIndex = context.windIndex
+      state.wind = context.wind
+      state.playerKey = context.playerKey
+      state.playerName = context.playerName
+      state.firstPlayer = context.firstPlayer
+      state.roomName = context.roomName
+      state.roomState = context.state
+      state.turn = context.turn
+      state.players = context.players
+      state.hand = context.hand
+      state.rivers = context.rivers
+      state.windPositions = buildWindPositions(state, context.windIndex)
+    }
   },
   getters: {
 
   }
+}
+
+/**
+ * @description 風とフィールド位置の組み合わせ情報を組み立てます。
+ * @returns {Object} 風とフィールド位置の組み合わせ情報
+ */
+function buildWindPositions (windIndex) {
+  const windPositions = {}
+  const position = ['lower', 'right', 'upper', 'left']
+  for (let i = 0; i < position.length; i++) {
+    windPositions[position[i]] = (windIndex + i) % 4
+  }
+  return windPositions
 }
